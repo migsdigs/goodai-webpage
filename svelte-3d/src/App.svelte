ca564d33693da41affe5501990485e510425674e
migsdigs <miguelgarcianaude@gmail.com>
2023-11-21 10:26:24 +0200

    nearing final changes

--- svelte-3d/src/App.svelte
+++ svelte-3d/src/App.svelte
@@ -2,12 +2,18 @@
 	import { onMount } from 'svelte';
 	import * as THREE from 'three';
 	import * as SC from 'svelte-cubed';
+	import {
+		Header,
+		HeaderNav,
+		HeaderNavItem,
+	} from "carbon-components-svelte"
 
 	let name = "GoodAI";
 	let socket;
 	let frequency = 10;
 	let size = 0.5;	// assign this an appropriate starting size
 	let updatedFrequency = 10;
+	let src = "images/logo-white.svg";
 
 	// for spin of the 3D components
 	let spin1 = 0;
@@ -19,12 +25,6 @@
 		spin2 += 0.02;
 		spin3 += 0.005;
 	});
-
-	// On spawn
-	onMount(() => {
-
-		
-	}); // End of onMount
 
 	// Connect to websocket url and log connection message
 	socket = new WebSocket('ws://0.0.0.0:8080/ws');
@@ -48,14 +48,20 @@
 		socket.send(JSON.stringify({ frequency }));
 	};
 
-
-	// function to update frequency value (also does not allow frequency value to be <1 and >30)
+	
+	/**
+	 * Update frequency value (also does not allow frequency value to be <1 and >30)
+	 */
 	function updateFrequencyAfterInput () {
 		// get value in the text
 		updatedFrequency = document.getElementById("frequencyText").value;
 
+		// If input box is empty frequency remains as it was until it is updated with valid value
+		if (updatedFrequency == '') {
+			frequency = frequency;
+		}
 		// check if it is outside the desired range (1-30)
-		if (updatedFrequency > 30 || updatedFrequency < 1) {
+		else if (updatedFrequency > 30 || updatedFrequency < 1) {
 			frequency = frequency;
 			updatedFrequency = frequency;
 			document.getElementById("frequencyText").value = frequency;
@@ -76,8 +82,8 @@
 <!-- 3D components -->
 
 
-<div class="container">
-	<SC.Canvas antialias background={new THREE.Color('papayawhip')}>
+<div class="grid-container">
+	<SC.Canvas antialias background={new THREE.Color("rgb(34,34,36)")}>
 		
 		<!--Cube 3D component  -->
 		<SC.Mesh 
@@ -85,7 +91,6 @@
 			material={new THREE.MeshStandardMaterial({ color: 0x03bafc })}
 			scale={[size, size, size]}
 			rotation={[spin1, spin1, spin1]}
-			castshadow
 			position={[-2.5,0,0]}
 		/>
 
@@ -103,15 +108,14 @@
 			material={new THREE.MeshStandardMaterial({ color: 0xa670db })}
 			scale={[size/2, size, size/2]}
 			rotation={[spin3, spin3, spin3]}
-			castshadow
 			position={[2.5,0,0]}
 		/>
 		
 		<SC.PerspectiveCamera position={[0, 0, 10]} />
 		<SC.OrbitControls
 			target={[0, 0, 0]}
-			enableZoom={false}
-			enablePan={false}
+			enableZoom={true}
+			enablePan={true}
 			enableDamping
 			maxPolarAngle={Math.PI * 0.5}
 		/>
@@ -119,38 +123,92 @@
 		<SC.DirectionalLight intensity={0.2} position={[-2, 3, 2]} />
 	</SC.Canvas>
 		
-</div>
+		
+</div>  <!-- End of grid-container div -->
+
+
+
+<!-- Header -->
+
+<svelte:head>
+  <link
+    rel="stylesheet"
+    href="https://unpkg.com/carbon-components-svelte/css/white.css"
+  />
+</svelte:head>
+
+<Header company="GoodAI" platformName="Safe Artificial Intelligence">
+
+</Header>
+
+<slot />
 
 
 <div class="block">
-	<h1>Hello {name}!</h1>
+	<img src={src} alt="GoodAI" />
+	<!-- <h1>Hello {name}!</h1> -->
 	<div>
 		<label for="frequency_label">Frequency:</label>
 		<input id="frequencyText" type="number" bind:value={updatedFrequency} on:input={updateFrequencyAfterInput} min="1" max="30">
 	</div>
-	
 </div>
+
+<div class="instructions">
+	<p>Left click and move mouse to rotate shapes.</p>
+	<p>Hold left and right click to pan camera.</p>
+	<p>Left click and move mouse to rotate shapes.</p>
+</div>
+
+
 
 
 
 <!-- Styling -->
 <style>
+	.block {
+		position: absolute;
+		top: 20%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+	}
+
+	.instructions {
+		color: aliceblue;
+		position: fixed;
+		left: 50%;
+		bottom: 20px;
+		transform: translate(-50%, -50%);
+		margin: 0 auto;
+	}
+
 	div {
         margin-bottom: 10px;
+		padding: 15px;
+		margin: auto;
       }
       label {
 		font-size: larger;
         display: inline-block;
         width: 110px;
-        color: #777777;
-      }
-      input {
-        padding: 5px 10px;
-		width: 60px;
-		border: 1px solid #000000;
+        color: aliceblue;
       }
 
-	.container {
+	input[type=number] {
+		width: 5em;
+		padding: 0.5em;
+		border: .2em solid #7fbb42;
+		border-radius: 1em;
+		text-align: center;
+		color: black;
+		appearance: textfield;
+		margin: 0;
+		&::-webkit-inner-spin-button {
+			opacity: 1;
+			background: red;
+		}
+	}
+
+	.grid-container {
 		position: absolute;
 		background: rgb(58, 63, 126);
 		width: 100%;
@@ -158,12 +216,11 @@
 		top: 0;
 	}
 
-
-	.block {
-		position: absolute;
-		top: 20%;
-		left: 50%;
-		transform: translate(-50%, -50%);
+	h1 {
+		color: white;
 	}
+
+
+	
 
 </style>
--- webapp/server.py
+++ webapp/server.py
@@ -22,7 +22,7 @@ async def websocket_handler(request: Request) -> WebSocketResponse:
 
     ws = WebSocketResponse()    # Create instance of WebSocket
     await ws.prepare(request)       # Wait for request
-    
+
 
     async def send_data():
         while True:
@@ -41,19 +41,7 @@ async def websocket_handler(request: Request) -> WebSocketResponse:
     async for msg in ws:
         if msg.type == WSMsgType.TEXT:
 
-            # frequency = update_frequency(msg=msg, frequency=frequency)
-            data = json.loads(msg.data)             # Load JSON data from request
-            data_frequency = data['frequency']      # Extract frequency value
-            # data = msg.data
-
-            # Update frequency value if it is not equal to 0 and greater than 0.
-            try:
-                if float(data_frequency) != 0 and float(data_frequency) >= 0:
-                    frequency = float(data_frequency)
-                    print(f"Frequency: {int(frequency)} Hz")
-            except TypeError:
-                print(data)
-                print("Frequency given was not a number.")
+            frequency = await update_frequency(msg=msg, frequency=frequency)
 
     return ws
 
@@ -64,36 +52,35 @@ app = web.Application()
 app.add_routes([web.get('/ws', websocket_handler)])
 
 
+# Function declarations
+async def update_frequency(msg: WSMsgType.TEXT, frequency: int) -> int:
+    """
+    Function to update the frequency at which server sends messages to client.
+
+    Attributes:
+        msg (WSMsgType.TEXT): Websocket message of text type 
+        containing the new frequency value.
+
+        frequency (int): current frequency value
+
+    Returns:
+        frequency int with new frequency value
+    """
+
+
+    data = json.loads(msg.data)             # Load JSON data from request
+    data_frequency = data['frequency']      # Extract frequency value
+
+    # Update frequency value if it is not equal to 0 and greater than 0.
+    try:
+        if float(data_frequency) != 0 and float(data_frequency) >= 0:
+            print(f"Frequency: {int(data_frequency)} Hz")
+            return float(data_frequency)
+    except TypeError:
+        print(data)
+        print("Frequency given was not a number.")
+        return frequency
+
+
 if __name__ == '__main__':
     web.run_app(app)
-
-
-
-# # Function declarations
-# def update_frequency(msg: WSMsgType.TEXT, frequency: int) -> int:
-#     """
-#     Function to update the frequency at which server sends messages to client.
-
-#     Attributes:
-#         msg (WSMsgType.TEXT): Websocket message of text type 
-#         containing the new frequency value.
-
-#         frequency (int): current frequency value
-
-#     Returns:
-#         frequency int with new frequency value
-#     """
-
-
-#     data = json.loads(msg.data)             # Load JSON data from request
-#     data_frequency = data['frequency']      # Extract frequency value
-
-#     # Update frequency value if it is not equal to 0 and greater than 0.
-#     try:
-#         if float(data_frequency) != 0 and float(data_frequency) >= 0:
-#             print(f"Frequency: {int(data_frequency)} Hz")
-#             return data_frequency
-#     except TypeError:
-#         print(data)
-#         print("Frequency given was not a number.")
-#         return frequency
